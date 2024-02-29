5aaaaffd678ce9b9305417cb269fadecb84e245c
Artur Lekki <a.lekki1990@gmail.com>
2024-02-29 12:32:15 +0100

    Dodaj pliki projektów.

---
+++ ToDoList/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ToDoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBox_AllTasks = new System.Windows.Forms.ListBox();
+            this.TextBox_NewTask = new System.Windows.Forms.TextBox();
+            this.Button_AddNewTask = new System.Windows.Forms.Button();
+            this.Button_DeleteTask = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ListBox_AllTasks
+            // 
+            this.ListBox_AllTasks.FormattingEnabled = true;
+            this.ListBox_AllTasks.ItemHeight = 16;
+            this.ListBox_AllTasks.Location = new System.Drawing.Point(32, 259);
+            this.ListBox_AllTasks.Name = "ListBox_AllTasks";
+            this.ListBox_AllTasks.Size = new System.Drawing.Size(516, 308);
+            this.ListBox_AllTasks.TabIndex = 0;
+            // 
+            // TextBox_NewTask
+            // 
+            this.TextBox_NewTask.Location = new System.Drawing.Point(32, 81);
+            this.TextBox_NewTask.Multiline = true;
+            this.TextBox_NewTask.Name = "TextBox_NewTask";
+            this.TextBox_NewTask.Size = new System.Drawing.Size(516, 136);
+            this.TextBox_NewTask.TabIndex = 1;
+            // 
+            // Button_AddNewTask
+            // 
+            this.Button_AddNewTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Button_AddNewTask.Location = new System.Drawing.Point(577, 81);
+            this.Button_AddNewTask.Name = "Button_AddNewTask";
+            this.Button_AddNewTask.Size = new System.Drawing.Size(195, 75);
+            this.Button_AddNewTask.TabIndex = 2;
+            this.Button_AddNewTask.Text = "Dodaj zadanie";
+            this.Button_AddNewTask.UseVisualStyleBackColor = true;
+            this.Button_AddNewTask.Click += new System.EventHandler(this.Button_AddNewTask_Click);
+            // 
+            // Button_DeleteTask
+            // 
+            this.Button_DeleteTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Button_DeleteTask.Location = new System.Drawing.Point(577, 492);
+            this.Button_DeleteTask.Name = "Button_DeleteTask";
+            this.Button_DeleteTask.Size = new System.Drawing.Size(195, 75);
+            this.Button_DeleteTask.TabIndex = 3;
+            this.Button_DeleteTask.Text = "Usuń zaznaczony";
+            this.Button_DeleteTask.UseVisualStyleBackColor = true;
+            this.Button_DeleteTask.Click += new System.EventHandler(this.Button_DeleteTask_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(30, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nowe zadanie:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(30, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Lista zadań:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 606);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Button_DeleteTask);
+            this.Controls.Add(this.Button_AddNewTask);
+            this.Controls.Add(this.TextBox_NewTask);
+            this.Controls.Add(this.ListBox_AllTasks);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBox_AllTasks;
+        private System.Windows.Forms.TextBox TextBox_NewTask;
+        private System.Windows.Forms.Button Button_AddNewTask;
+        private System.Windows.Forms.Button Button_DeleteTask;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ToDoList/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        private List<string> _tasks = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // ustaw formualrz na środku ekranu
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            // Pobranie wersji zestawu Assembly
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            // Ustawienie wersji jako tytułu formularza
+            this.Text = $"ToDoList v: {version.Major}.{version.Minor}.{version.Build}";
+        }
+
+        public void RefreshListBox()
+        {
+            // Wyczyść kontrolke listBox
+            ListBox_AllTasks.Items.Clear();
+
+            // Dodaj do kontrolki listBox zadania z głównej listy
+            for(int i = 0; i < _tasks.Count; i++)
+            {
+                ListBox_AllTasks.Items.Add(_tasks[i]);
+            }
+        }
+
+        private void Button_AddNewTask_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(TextBox_NewTask.Text))
+            {
+                _tasks.Add(TextBox_NewTask.Text);
+                TextBox_NewTask.Text = "";
+                RefreshListBox();
+            }
+        }
+
+        private void Button_DeleteTask_Click(object sender, EventArgs e)
+        {
+            // Pobierz zaznaczony element
+            object isSelected = ListBox_AllTasks.SelectedItem;
+
+            // Sprawdź czy jakiekolwiek zadanie jest zaznaczone czy jest nie
+            if(isSelected != null )
+            {
+                // Stwórz potwierdzenie usunięcia
+                DialogResult confirmDelete = MessageBox.Show("Czy na pewno usunąć " + isSelected + "?", "Potwierdź", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                // Sprawdź czy został kliknięty klawisz YES
+                if(confirmDelete == DialogResult.Yes)
+                {
+                    // tymczasowa lista
+                    List<string> tempList = new List<string>();
+                    object selectedItem;
+
+                    // skopiuj elementy z kontrolki listbox do tymczasowej listy
+                    foreach (string task in ListBox_AllTasks.Items)
+                    {
+                        tempList.Add(task);
+                    }
+
+                    // z tymczasowej listy pobierz tylko ten zaznaczony element
+                    selectedItem = ListBox_AllTasks.SelectedItem;
+
+                    // z tymczasowej listy usuń tylko ten zaznaczony
+                    tempList.Remove(selectedItem.ToString());
+
+                    // z głównej listy usuń wszystko
+                    _tasks.Clear();
+
+                    // z tymczasowej listy dodaj do główej listy zaktualizowane elementy
+                    foreach (string task in tempList)
+                    {
+                        _tasks.Add(task);
+                    }
+
+                    RefreshListBox();
+                }
+            }
+        }
+    }
+}
